35384989673603f61e474a7c03d6efb20363d781
Enkh-Anar Erdenetsogt <ng77b0@inf.elte.hu>
2022-12-22 22:51:29 +0100

    Made models and migrations

---
+++ recipe_browser/database/migrations/2022_12_22_174911_create_favorites_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use App\Models\Recipe;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->constrained();
+            $table->unsignedBigInteger('recipe_id')->unsigned();
+            $table->foreign('recipe_id')->references('id')->on('recipes')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
